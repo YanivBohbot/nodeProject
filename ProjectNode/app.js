b99ac8dce627a10d6680b278863649393236014b
YanivBohbot <Yanivbohbot5@gmail.com>
2021-10-31 21:38:53 +0200

    again

--- ProjectNode/app.js
+++ ProjectNode/app.js
@@ -21,7 +21,6 @@ app.use('/',(req, res, next) => {
 });
 
 
-<<<<<<< HEAD
 app.use('/',(req, res, next) => {
     console.log('In the middleware!');
     res.send('<h1> the Add prodcut</h1>');
@@ -45,9 +44,6 @@ app.use('/',(req, res, next) => {
 
 
 
-=======
-
->>>>>>> features/login
 const server = http.createServer(app);
 
 server.listen(3000);

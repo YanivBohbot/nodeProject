6a362eb35d9c22832d32c47f005b1b2fb74a021f
YanivBohbot <Yanivbohbot5@gmail.com>
2021-10-31 19:03:21 +0200

    fix structure of files

--- ProjectNode/app.js
+++ ProjectNode/app.js
@@ -2,28 +2,22 @@ const http = require('http');
 
 const express = require('express');
 
-const app = express();
+app.use('/' , (req,res,next)=>{
+    console.log("hello");
+    next();
+})
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
     console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
-});
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
-});
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
-});
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+    res.send('<h1> the Add prodcut</h1>');
     next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+
+app.use('/',(req, res, next) => {
+    console.log('In the middleware!');
+    res.send('<h1> the Add prodcut</h1>');
+   
 });
 
 const server = http.createServer(app);
